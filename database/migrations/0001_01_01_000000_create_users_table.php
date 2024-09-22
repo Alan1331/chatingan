7179cb5966384da0aea65d9d6fd63051d41092de
Alan1331 <=>
2024-09-22 20:00:05 +0700

    feat(database/users): adjust migration for users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address');
+            $table->string('gender');
+            $table->string('marital_status');
             $table->rememberToken();
             $table->timestamps();
         });
