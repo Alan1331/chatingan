581968da3b077e3cca89287d2bda4d4913f64afb
Alan1331 <=>
2024-09-22 20:16:11 +0700

    fix(database/users): change datatype of gender from string to boolean

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('address');
-            $table->string('gender');
+            $table->boolean('gender');
             $table->string('marital_status');
             $table->rememberToken();
             $table->timestamps();
